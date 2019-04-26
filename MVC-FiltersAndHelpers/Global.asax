67fb71520cf4a3aa52c49ac5f0f90eaa295096fd
Dilraj Singh <dilrajkehra@gmail.com>
2019-04-26 09:05:48 -0500

    Add project files.

---
+++ MVC-FiltersAndHelpers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_FiltersAndHelpers.MvcApplication" Language="C#" %>
---
+++ MVC-FiltersAndHelpers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_FiltersAndHelpers.Startup))]
+namespace MVC_FiltersAndHelpers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
